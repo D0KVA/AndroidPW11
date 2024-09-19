935f45c500fa5ab700815d02cc70312134c4afd5
D0KVA <isip_v.s.sokolov@mpt.ru>
2024-09-19 08:59:26 +0300

    i hate my life

---
+++ app/src/main/java/com/example/pw11/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.pw11;
+
+    import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnFrameAnimation, btnTweenAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFrameAnimation = findViewById(R.id.btnFrameAnimation);
+        btnTweenAnimation = findViewById(R.id.btnTweenAnimation);
+
+        Animation buttonSlideIn = AnimationUtils.loadAnimation(this, R.anim.button_slide_in);
+
+        btnFrameAnimation.startAnimation(buttonSlideIn);
+        btnTweenAnimation.startAnimation(buttonSlideIn);
+
+        btnFrameAnimation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FrameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnTweenAnimation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TweenActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
